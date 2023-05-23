7c02f33ba036ac6a187d80f0bd1ac3ef19db7c1e
guiijanuario <guilherme.januario@zup.com.br>
2023-05-23 17:32:52 -0300

    Acrescentando no exercicio 1 a quantidade de kilos excedente

--- src/exercicio1.java
+++ src/exercicio1.java
@@ -16,6 +16,8 @@ public class exercicio1 {
 
         if(pesoExcedente > 0){
             double multaPesoExcedente = pesoExcedente * 4;
+
+            System.out.println("O peso excedente é de : " + formatador.format(pesoExcedente) + "kg");
             System.out.println("Ele precisa pagar de multa: " + "R$ " + formatador.format(multaPesoExcedente));
         }else{
             System.out.println("Ele Não precisa pagar multa!");
